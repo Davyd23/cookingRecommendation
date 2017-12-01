671bd0f081114edb05eba5dc3d01d68039797321
Davyd23 <daviddedu02@gmail.com>
2017-12-01 20:34:28 +0200

    created project structure

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Best Cooking App
+    </title>
+    <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.css">
+</head>
+<body ng-app="cookingRecommendation">
+
+    <div ng-include="'view/main.php'"></div>
+
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/angular/angular.js"></script>
+
+    <script src="script/application.js"></script>
+    <script src="script/controller/mainController.js"></script>
+</body>
+</html>
+
+
+
+
+
+
 <?php
-    echo "hello";
+
 ?>
---
+++ view/main.php
@@ -0,0 +1,6 @@
+<div ng-controller="MainController">
+    <span ng-bind="test"></span>
+    <div id="topBar"></div>
+    <div id="leftPanel"></div>
+    <div id="rightPanel"></div>
+</div>
