52ae438824a64d77d91cff166fee59667d0de438
Davyd23 <daviddedu02@gmail.com>
2017-12-01 23:41:34 +0200

    simple side bar menu added

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
         Best Cooking App
     </title>
     <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/simple-sidebar.css">
 </head>
 <body ng-app="cookingRecommendation">
 
     <div ng-include="'view/main.php'"></div>
 
     <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="bower_components/angular/angular.js"></script>
 
     <script src="script/application.js"></script>
--- view/main.php
+++ view/main.php
@@ -1,6 +1,9 @@
-<div ng-controller="MainController">
-    <span ng-bind="test"></span>
+<div ng-controller="MainController" id="wrapper">
     <div id="topBar"></div>
-    <div id="leftPanel"></div>
-    <div id="rightPanel"></div>
+    <div id="leftPanel">
+        <div ng-include="'view/leftPanel.php'"></div>
+    </div>
+    <div id="rightPanel">
+        <div ng-include="'view/rightPanel.php'"></div>
+    </div>
 </div>
